7017b8651fb93bada453dda94b4b738cab171b5a
pratyush kamani <79060993+pratyush06@users.noreply.github.com>
2025-09-30 19:03:17 +0530

    WIP for solution 11085

---
+++ Backtracking/uva-11085-back-to-8-queens-problem.py
@@ -0,0 +1,54 @@
+import copy
+
+def place(curr_idx, current_pos):
+    # import pdb;pdb.set_trace()
+    if (curr_idx in current_pos):
+        return False
+    
+    else:
+        for i in range(len(current_pos)):
+            if abs(curr_idx - current_pos[i]) == abs(len(current_pos) - i):
+                return False
+    return True
+
+def backtrack(queen_pos, current_pos=[], ans=None, current_count=0):
+    # print(f" length of current pos {current_pos}")
+    if  len(current_pos)==8:
+        if current_count==8:
+            import pdb;pdb.set_trace()
+        if not ans or current_count<ans:
+            ans=current_count
+        # temp_ans = 0
+        # for k, j in enumerate(queen_pos):
+        #     if current_pos[k]!=k:
+        #         temp_ans+=1
+        
+        # if not ans or temp_ans<ans:
+        #     ans = temp_ans
+
+    for i in range(1, 9):
+        if place(i, current_pos):
+            if i==1 and current_pos==[2, 4, 6, 8, 3]:
+                import pdb;pdb.set_trace()
+            if i!=queen_pos[len(current_pos)]:
+                current_count+=1
+            current_pos.append(i)
+            ans=backtrack(queen_pos, current_pos, ans, current_count)
+            current_pos.pop()
+            current_count-=1
+    return ans
+
+case_no = 1
+while True:
+    queen_pos = list(input().split(" "))
+    # import pdb;pdb.set_trace()
+    if queen_pos[0]=="":
+        break
+    queen_pos = list(map(int, queen_pos))
+    # queen_pos = [None]+queen_pos
+    queen_pos = [1,1,1,1,1,1,1,1]
+    new=backtrack(queen_pos)
+
+    # import pdb;pdb.set_trace()
+    print(f"Case {case_no}: {new}")
+    case_no+=1
