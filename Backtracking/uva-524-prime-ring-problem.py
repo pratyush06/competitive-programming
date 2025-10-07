92b4778950257b6bfeb81d6203994411d5228ff4
pratyush06 <pratyushkamani54@gmail.com>
2025-10-08 00:04:18 +0530

    solved prime ring problem

---
+++ Backtracking/uva-524-prime-ring-problem.py
@@ -0,0 +1,44 @@
+import copy
+def place(idx, curr, n):
+    if idx in curr:
+        return False
+    
+    second_last = curr[-1]
+    summation = second_last+idx
+    for i in range(2, summation):
+        if summation%i==0:
+            return False
+    if len(curr)==n-1:
+        # import pdb;pdb.set_trace()
+        summation = idx+1
+        for i in range(2, summation):
+            if summation%i==0:
+                return False
+    return True
+
+
+
+def backtrack(n, curr=None, ans=None):
+    if curr is None:
+        curr=[1,]
+    if ans is None:
+        ans=[]
+    if len(curr)==n:
+        # import pdb;pdb.set_trace()
+        ans.append(copy.copy(curr))
+    # exit condition
+
+    # import pdb;pdb.set_trace()
+    for i in range(2,n+1):
+        if place(i, curr, n):
+            curr.append(i)
+            ans = backtrack(n, curr, ans)
+            # print(curr)
+            curr.pop()
+
+    return ans
+
+
+
+n = int(input())
+print(backtrack(n))
